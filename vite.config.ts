f5224a25a3f9575e0f62ef498705c1f8b9da354c
Левков Глеб <levkov.gleb.dev@gmail.com>
2024-05-24 23:33:01 +0300

    feat: default server port - 3000

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [preact(), svgr()],
+    server: {port: 3000},
     optimizeDeps: {
         extensions: ['.css'],
         esbuildOptions: {
